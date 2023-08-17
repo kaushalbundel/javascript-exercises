511c30127cf2599be96ed2cdfdf2cb300ecc1276
Kaushal Bundel <kaushalbundel@gmail.com>
2023-08-17 17:13:00 +0530

    Problem 1 and 2 Solved.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, repeat) {
+    let emptyString = ""
+if (repeat === 0){
+    emptyString =  "";
+}
+else if (repeat < 0){
+    emptyString = "ERROR";
+}
+else {
+    for(let i = 0; i < repeat; i++){
+        emptyString = emptyString + string;
+}
+}
+return emptyString;}
 
 // Do not edit below this line
 module.exports = repeatString;
